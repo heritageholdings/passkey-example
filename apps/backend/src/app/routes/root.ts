ee4de0f9a740ab8e504d70afabb9b06dd984992a
Fabrizio <26501317+fabriziofff@users.noreply.github.com>
2023-11-30 00:18:03 +0100

    testing nx

---
+++ apps/backend/src/app/routes/root.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify';
+
+export default async function (fastify: FastifyInstance) {
+  fastify.get('/', async function () {
+    return { message: 'Hello API' };
+  });
+}
